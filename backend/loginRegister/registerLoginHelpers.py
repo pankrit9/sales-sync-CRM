cc9c5b857b6e3a1edbe6b4c6f576836f0a3d3690
Alejandro Stevan Posada Ciro <Alejandronoguchi@gmail.com>
2023-06-18 21:13:45 +1000

    Merged pantrik's frontend into backend

--- backend/loginRegister/registerLoginHelpers.py
+++ backend/loginRegister/registerLoginHelpers.py
@@ -1,10 +1,4 @@
-
-def check_email_taken(email):
-
-
-    return False
-
-
+import re
 # Checks user's email to see whether it is valid or not
 def check_email(email):
     # Regular expression for validating an email
@@ -12,27 +6,27 @@ def check_email(email):
     
     # Email length cannot be 0 or greater than 254
     if len(email) == 0 or len(email) > 254:
-        raise InputError(description='Invalid email length')
+        raise Exception(description='Invalid email length')
     
     # Checks if invalid email format
     if not re.fullmatch(regex, email):
-        raise InputError(description='Invalid email format')
+        raise Exception(description='Invalid email format')
 
 # Checks if an email address is already being used by another user
 def check_duplicate_email(email, store):
     for user in store['users']:
         if user['email'] == email:
-            raise InputError(description='Entered email already exists')
+            raise Exception(description= 'The given email is already taken')
 
 # Checks if user's password less than 6 characters (invalid)
 def check_password_lenght(password):
     if len(password) < 8:
-        raise InputError(description='Password cannot be less than 6 characters')
+        raise Exception(description='Password cannot be less than 8 characters')
 
 # Checks if length of user's first and last name is not between 1 and 50 characters inclusive (invalid)
 def check_name(name):
     if len(name) < 1 or len(name) > 50:
-        raise InputError(description='Invald name length')
+        raise Exception(description='Invalid name length')
 
 # Check data_store to see if generated handle is already in use
 # Return True if handle is already in use, otherwise return False
--- backend/server.py
+++ backend/server.py
@@ -1,27 +1,32 @@
 import json
-from flask import Flask , render_template, request, flash, jsonify
+import datetime
 import jwt
+from flask import Flask, render_template, request, flash, jsonify, session
 from functools import wraps
 from pymongo import MongoClient
-from helpers import add_customer
-import datetime
-
+from flask_cors import CORS
 from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
+CORS(app)
 app.config['SECRET_KEY'] = 'Avengers'
 
-database_URL = "mongodb+srv://avengers:endgame@crm.e8aut5k.mongodb.net/"
-client = MongoClient(database_URL)
+
 SECRET_JWT = 'salesync'
 
+
 # db is the CRM database
+database_URL = "mongodb+srv://avengers:endgame@crm.e8aut5k.mongodb.net/"
+client = MongoClient(database_URL)
+
 db = client.CRM
 
 # bcrypt is going to be used for password encryption
 # https://www.geeksforgeeks.org/password-hashing-with-bcrypt-in-flask/
 bcrypt = Bcrypt(app)
 
+
+# This might be usefull later on
 def token_required(f):
     '''
     This is going to be the wrapper function that will called before accessing any route 
@@ -33,7 +38,7 @@ def token_required(f):
 
         if not token:
             return jsonify({'message': 'Token not found'}), 403
-        
+
         try:
             data = jwt.decode(token, app.config['SECRET_KEY'])
         except:
@@ -41,71 +46,67 @@ def token_required(f):
     return decorated
 
 
-
-@app.route("/" , methods=['GET'])
+@app.route("/", methods=['GET'])
 def home():
-    return render_template("home.html")
+    return
 
 
-@app.route("/login" , methods=['GET','POST'])
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    request.form['email']
-    request.form['password']
-    matching_user = db.Accounts.find_one({"email": request.form['email']})
 
-    # Encrypt the password
-    encoded_password = bcrypt.generate_password_hash(request.json['password']).decode('utf-8')
+    # find user with the given email
+    matching_user = db.Accounts.find_one({"email": request.json['email']})
 
-
-    if request.form['email'] and matching_user['password'] == encoded_password:
-
-        # Created a token so users can be authenticated 
+    if bcrypt.check_password_hash(matching_user['password'], request.json['password']):        
+        # Created a token so users can be authenticated
         token = jwt.encode({
-            'user': request.form['email'],
-            'position': request.json['position'],
-            'exp' : datetime.datetime.utcnow() + datetime.timedelta(seconds=180)
-        }, app.config['SECRET_KEY']) #Secret key is 'Avengers'
+            'user': request.json['email'],
+            'first_name': matching_user['first_name'],
+            'position': matching_user['position'],
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=180)
+        }, app.config['SECRET_KEY'])  # Secret key is 'Avengers'
 
-        return jsonify({'token': token.decode('utf-8')})
-    else:
-        pass
-        
+        # return token for the session
+        return jsonify({'token': token})
 
 
 @app.route("/register", methods=['POST'])
 def register():
 
     # Encrypt the password
-    encoded_password = bcrypt.generate_password_hash(request.json['password']).decode('utf-8')
+    encoded_password = bcrypt.generate_password_hash(
+        request.json['password']).decode('utf-8')
 
-    # Create a temporary user that will be added to the mongodb  
-    new_user = { 
-        "email" : request.json['email'],
-        "first_name" : request.json['first_name'],
-        "last_name" : request.json['last_name'],
-        "password" : encoded_password,
-        "position" : request.json['position'],
-        "company" : request.json['company']
+    # Create a temporary user that will be added to the mongodb
+    new_user = {
+        "email": request.json['email'],
+        "first_name": request.json['first_name'],
+        "last_name": request.json['last_name'],
+        "password": encoded_password,
+        "position": request.json['position'],
+        "company": request.json['company']
     }
 
     # Check whether the email is taken
     if db.Accounts.find_one({"email": new_user['email']}):
         return jsonify({"error": "The given email is already taken."})
-    
+
     # Adds user to the database
     db.Accounts.insert_one(new_user)
 
     # Returns the success message, Should return token
-    return jsonify({"msg" :"User was succesfuly created."})
+    return jsonify({"msg": "User was succesfuly created."})
+
 
 @app.route("/users", methods=['GET'])
-#@token_required
+# @token_required
 def staff_list():
     users_cursor = db.Accounts.find()
     users_list = [user for user in users_cursor]
     return jsonify({
-            
-        })
+
+    })
+
 
 if __name__ == '__main__':
     app.run(debug=True)
