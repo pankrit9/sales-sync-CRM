5706ff74d8e8dea15ad7d1f9f6e31da0684c9744
Alejandro Stevan Posada Ciro <Alejandronoguchi@gmail.com>
2023-06-28 02:52:59 +1000

    added css for searchbar

--- frontend/src/pages/products.jsx
+++ frontend/src/pages/products.jsx
@@ -5,10 +5,14 @@ import EnhancedTable from "../components/productsTable";
 import AddBtn from "../components/addProdBtn";
 import EditBtn from "../components/editProdBtn";
 import { BACKEND_API } from "../api";
+import { SearchBar } from '../components/SearchBar';
+import "../components/Searchbar.css"
 
 function Products() {
     
     const [products, setProducts] = useState([]);
+    const [searchQuery, setSearchQuery] = useState("");
+    
 
     const fetchData = useCallback(async () => {
         const response = await fetch(`${BACKEND_API}/products`, {method: "GET"});
@@ -16,22 +20,33 @@ function Products() {
         setProducts(data);
     }, []);
 
+    const filterData = (query, products) => {
+        if (!query) {
+          return products;
+        } else {
+          return products.filter((d) => d['name'].toLowerCase().includes(query));
+        }
+    };
+
     useEffect(() => {
         fetchData();
     }, [fetchData]);
-    
+
+    const dataFiltered = filterData(searchQuery, products);
     return (
         <>
             <Navbar/>
             <h1 className="header" style={{paddingLeft: '140px', marginTop: '50px', fontSize: '60px'}}>Products</h1>
-            <div style={{display: 'flex', marginLeft: '1070px'}}>
-                <div style={{marginRight: '10px'}}>
-                    <EditBtn fetchData={fetchData}/>
+            <div className="tools">
+                <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery}  />
+                <div className='product-buttoms'>
+                    <EditBtn fetchData={fetchData} style={{right:'10%'}}/>
+                    <AddBtn fetchData={fetchData}/>
                 </div>
-                <AddBtn fetchData={fetchData}/>
+                
             </div>
             <div style={{ marginLeft:'140px', marginRight: '120px', marginTop: '50px'}}>
-                {products.length > 0 ? <EnhancedTable rows={products} fetchData={fetchData}/> : <p>Loading...</p>}
+                {dataFiltered.length > 0 ? <EnhancedTable rows={dataFiltered} fetchData={fetchData}/> : <p>The inventory is empty</p>}
             </div>
         </>
     );
