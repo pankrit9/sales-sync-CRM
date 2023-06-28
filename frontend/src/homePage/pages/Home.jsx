be641ce870b2258f86ba812de7ae908451a0ba1d
Alejandro Stevan Posada Ciro <Alejandronoguchi@gmail.com>
2023-06-29 03:44:24 +1000

    fixing landing page

--- frontend/src/homePage/pages/Home.jsx
+++ frontend/src/homePage/pages/Home.jsx
@@ -3,10 +3,12 @@ import '../../App.css';
 import Cards from '../Cards';
 import HeroSection from '../HeroSection';
 import Footer from '../Footer';
+import Navbar2 from '../Navbar2';
 
 function Home() {
   return (
     <>
+      <Navbar2/>
       <HeroSection />
       <Cards />
       <Footer />
