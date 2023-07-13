803c0141a1de0a6df91ee1747dc33488d1f9bb8b
Alexander Burfoot <burfootalexander@gmail.com>
2023-07-13 15:32:31 +1000

    Services and Pricing

---
+++ frontend/src/homePage/ServicesComponents.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './ServicesComponents.css';
+
+const text = [
+  { title: 'Customer Resource Management does not have to be expensive', text: 'Get great value Customer Resource Management with Sale Sync' },
+  { title: 'Secure and Easy Sign Up', text: 'Sign Up and get managing your encrypted data within 5 minutes' },
+  { title: 'Simple to understand Dashboard', text: 'Have all the Information you need right at your fingertips' },
+  { title: 'Clear Task Management', text: 'Know what tasks you need to complete and when, with our clear task management system' },
+  { title: 'Manage your Clients and Staff', text: 'Know whos who with our clear tables highlighting important client and staff data' },
+  { title: 'Manage your Products', text: 'Understand what is in your inventory with our product management system' },
+  { title: 'Gamify your Customer Resource Management', text: "Don't get bored with pure data, enjoy our gamification to keep you and your staff motivated" },
+  { title: 'Dark Mode', text: 'Dark Mode helps you work at any hour you need' },
+];
+
+function Components() {
+  return (
+    <div>
+      {text.map((item, index) => {
+        const isReversed = index % 2 !== 0;
+        const isFirst = index === 0;
+        return (
+          <div className={`container ${isReversed ? 'reversed' : ''}`} key={index}>
+            <div className={`text ${isFirst ? 'first-text' : ''}`}>
+              <h1>{item.title}</h1>
+              <p>{item.text}</p>
+            </div>
+            <div className={`image image${index + 1}`}></div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default Components;
--- frontend/src/homePage/pages/Pricing.jsx
+++ frontend/src/homePage/pages/Pricing.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import '../../App.css';
-import Cards from '../Cards';
-import HeroSection from '../HeroSection';
-import Footer from '../Footer';
+import PricingComponents from '../PricingComponents';
 import Navbar2 from '../Navbar2';
 
 function Pricing() {
   return (
     <>
       <Navbar2/>
-      <Cards/>
+      <PricingComponents/>
     </>
   );
 }
