9e2cd46aef3d209a2368d25137db276c774cc931
Pankrit Jindal <pankritjindal30@gmail.com>
2023-08-03 18:01:00 +1000

    chatbot fixes

--- frontend/src/components/chatbot/Chatbot.jsx
+++ frontend/src/components/chatbot/Chatbot.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { AiOutlineMessage, AiOutlineClose } from 'react-icons/ai';
+import { AiOutlineClose } from 'react-icons/ai';
 // import SmartToyIcon from '@mui/icons-material/SmartToy';
 import SmartToyTwoToneIcon from '@mui/icons-material/SmartToyTwoTone';
 import SendTwoToneIcon from '@mui/icons-material/SendTwoTone';
@@ -57,20 +57,34 @@ function Chatbot() {
                             <div class="chatbox__image--header">
                                 <img src="https://img.icons8.com/color/48/000000/circled-user-female-skin-type-5--v1.png" alt="image"></img>
                             </div>
-                            <div class="chatbox__content--header">
-                                <h4 class="chatbox__heading--header">Chat support</h4>
-                                <p class="chatbox__description--header" style={{backgroundColor:theme.palette.neutral.main,padding:"0 0.5rem", margin:"1rem 0", borderRadius: "0.2rem", color:'black'}}>Hi. My name is Sam. How can I help you?</p>
+                            <div class="chatbox__content--header" style={{
+                                color: theme.palette.text.chatbot,
+                            }}>
+                                <h4 class="chatbox__heading--header" style={{color:theme.palette.neutral.main}}>Chat support</h4>
+                                <p class="chatbox__description--header" style={{backgroundColor:"#ccc", padding:"0 0.5rem", margin:"1rem 0", borderRadius: "0.2rem", color:'black'}}>Hi. My name is Sam. How can I help you?</p>
                             </div>
                         </div>
                         {messages.map((message, index) => (
-                            <div key={index} className={`chatbot-message chatbot-message-${message.author.toLowerCase()}`}>
+                            <div key={index} className={`chatbot-message chatbot-message-${message.author.toLowerCase()}`} style={{
+                                text: theme.palette.neutral.main,
+                            }}>
                                 {message.text}
                             </div>
                         ))}
                     </div>
                     <div className="chatbot-input" style={{ backgroundColor: theme.palette.neutral.main, zIndex: "888888" }}>
                         <input type="text" value={input} onChange={handleInputChange} />
-                        <button onClick={handleSend}><SendTwoToneIcon /></button>
+                        <button onClick={handleSend} style={{
+                            margin: "0 0.5rem",
+                            padding: "0.2rem",
+                            alignItems: "center",
+                            borderRadius: "0.2rem",
+                            cursor: "pointer"
+                        }}>
+                                <SendTwoToneIcon style={{
+                                    alignItems: "center",
+                                }}/>
+                        </button>
                     </div>
                 </div>
             )}
