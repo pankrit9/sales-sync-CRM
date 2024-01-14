9b531f93583c96043880dc32bbee8ddf1b1c10c1
Pankrit Jindal <pankritjindal30@gmail.com>
2024-01-14 16:14:43 +1100

    enrypted

--- backend/server.py
+++ backend/server.py
@@ -16,7 +16,7 @@ import os   # access to env variables using os.getenv
 from routes.chatbot_routes import chatbot, setupChatbot
 
 def create_app(host='0.0.0.0', port=6969):
-    os.environ["OPENAI_API_KEY"] = "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"
+    # os.environ["OPENAI_API_KEY"] = "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"
 
     load_dotenv()
 
