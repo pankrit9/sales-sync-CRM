912e8f38faeed76bed6f637707a08be4dba7f8b6
Pankrit Jindal <pankritjindal30@gmail.com>
2024-01-15 11:46:32 +1100

    domain added

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,2 +1,3 @@
-export const BACKEND_API = "http://52.64.82.221:6969";
+export const BACKEND_API = "https://api1.pankritjindal.com";
+// export const BACKEND_API = "http://52.64.82.221:6969";
 // export const BACKEND_API = "http://127.0.0.1:6969";
